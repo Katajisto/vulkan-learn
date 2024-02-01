2f086b099fe1b85bc6f7ea6fb8574bf4677dc809
Tuomas Katajisto <t@ktj.st>
2024-02-01 13:13:02 +0200

    Create vkinit.h

---
+++ vkinit.h
@@ -0,0 +1,14 @@
+//
+// Created by katajisto on 1.2.2024.
+//
+
+#ifndef VKBOOTPROJ_VKINIT_H
+#define VKBOOTPROJ_VKINIT_H
+#include "vulkan/vulkan.h"
+
+namespace vi {
+    VkImageSubresourceRange image_subresource_range(VkImageAspectFlags aspectMask);
+}
+
+
+#endif //VKBOOTPROJ_VKINIT_H
